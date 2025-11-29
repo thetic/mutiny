cpputest/cpputest

Rename config.h and HAVE_CONFIG_H

include/CppUTest/CppUTestGeneratedConfig.h
@@ -41,7 +41,6 @@
  *
  */
 
-#ifdef HAVE_CONFIG_H
+#ifdef HAVE_CPPUTEST_CONFIG_H
 #include "generated/CppUTestGeneratedConfig.h"
 #endif
-

thetic/mutiny

include/CppUTest/CppUTestGeneratedConfig.h
@@ -41,7 +41,7 @@
  *
  */
 
-#ifdef HAVE_CONFIG_H
+#ifdef CPPUTEST_HAVE_GENERATED_CONFIG_H
 #include "generated/CppUTestGeneratedConfig.h"
 #endif
 